64d3bd47464daddaf7b5496ff2804188d5895dcd
Sarah Cristina Ribeiro <100208442+projetoSarahRibeiro@users.noreply.github.com>
2022-11-29 09:05:02 -0300

    Add files via upload

--- funcao.js
+++ funcao.js
@@ -1,3 +1,38 @@
 function soma(a,b){
     return a+b
 }
+
+function subtr(a,b){
+    return a-b
+}
+
+function div(a,b){
+    return a/b
+}
+
+function multi(a,b){
+    return a*b
+}
+
+function decisao(resp, pri, seg){
+    if(resp == 1){
+        resultado = soma(pri,seg)
+        document.write("<p> A soma é "+ resultado)
+    }
+
+    if(resp == 2){
+        resultado = subtr(pri,seg)
+        document.write("<p> A subritação é "+ resultado)
+    }
+    
+    if(resp == 3){
+        resultado = div(pri,seg)
+        document.write("<p> A divisão é "+ resultado)  
+    }
+
+    if(resp == 4){
+        resultado = multi(pri,seg)
+        document.write("<p> A multiplicação é "+ resultado)
+    }
+
+}
